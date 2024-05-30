a8d3a8896c7b428e552199c8c4f6c86ed38f2a65
Kateryna <kateryna.babachanakh1@etu.univ-lorraine.fr>
2024-05-30 08:55:12 +0200

    Ajout la classe Arc.java

---
+++ src/Arc.java
@@ -0,0 +1,24 @@
+package src;
+
+public class Arc {
+    private String dest;
+    private double cout;
+
+    public Arc(String dest, double cout) {
+        this.dest = dest;
+        this.cout = cout;
+    }
+
+   public String toString(){
+
+        return "(" + this.dest + "," + this.cout + ")";
+   }
+
+   public String getDest(){
+        return this.dest;
+   }
+
+   public double getCout() {
+        return this.cout;
+    }
+}
